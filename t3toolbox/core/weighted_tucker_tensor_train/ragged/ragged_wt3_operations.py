b33eab85d69e829537d108d6d000c1084e72e5bd
NickAlger <nalger225@gmail.com>
2026-04-19 12:13:22 -0500

    wt3_probing works

--- t3toolbox/core/weighted_tucker_tensor_train/ragged/ragged_wt3_operations.py
+++ t3toolbox/core/weighted_tucker_tensor_train/ragged/ragged_wt3_operations.py
@@ -1,3 +1,7 @@
+# Authors: Nick Alger and Blake Christierson
+# Copyright: MIT License (2026)
+# Github: https://github.com/NickAlger/TuckerTensorTrainTools
+# Documentation: https://nickalger.github.io/TuckerTensorTrainTools/index.html
 import numpy as np
 import typing as typ
 
