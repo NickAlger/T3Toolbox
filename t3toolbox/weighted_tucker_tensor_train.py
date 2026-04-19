9eaa00bee1acb89d1b4e0803b9b6c4a69394e38b
NickAlger <nalger225@gmail.com>
2026-04-19 11:51:28 -0500

    wt3_apply works

--- t3toolbox/weighted_tucker_tensor_train.py
+++ t3toolbox/weighted_tucker_tensor_train.py
@@ -175,12 +175,17 @@ class WeightedTuckerTensorTrain:
             self.x0.data, self.edge_weights.data, use_jax=use_jax,
         ))
 
-    def squash_tails(self) -> 'WeightedTuckerTensorTrain':
-        result = ragged_wt3_operations.wt3_squash_tails(self.data)
+    def squash_tails(self, use_jax: bool = False) -> 'WeightedTuckerTensorTrain':
+        result = ragged_wt3_operations.wt3_squash_tails(self.data, use_jax=use_jax)
         return WeightedTuckerTensorTrain(
             t3.TuckerTensorTrain(*result[0]),
             EdgeVectors(*result[1]),
         )
+
+    def reverse(self) -> 'WeightedTuckerTensorTrain':
+        """Reverse the weighted Tucker tensor train.
+        """
+        return WeightedTuckerTensorTrain(self.x0.reverse(), self.edge_weights.reverse())
 
     def __neg__(self) -> 'WeightedTuckerTensorTrain':
         return WeightedTuckerTensorTrain(-self.x0, self.edge_weights)
@@ -221,6 +226,31 @@ class WeightedTuckerTensorTrain:
         return self.contract_edge_weights_into_cores(use_jax=use_jax).norm(
             use_orthogonalization=use_orthogonalization, use_jax=use_jax
         )
+
+    def to_dense(self, use_jax: bool = False) -> NDArray:
+        """Convert the weighted Tucker tensor train to a dense array.
+
+        Examples
+        --------
+        >>> import numpy as np
+        >>> import t3toolbox.tucker_tensor_train as t3
+        >>> import t3toolbox.weighted_tucker_tensor_train as wt3
+        >>> randn = np.random.randn
+        >>> x0 = t3.t3_corewise_randn((6,7,8), (5,6,7), (2,3,3,1), stack_shape=(4,))
+        >>> tucker_vectors = tuple([randn(4, 5), randn(4, 6), randn(4, 7)])
+        >>> tt_vectors = tuple([randn(4, 2), randn(4, 3), randn(4, 3), randn(4, 1)])
+        >>> weights = wt3.EdgeVectors(tucker_vectors, tt_vectors)
+        >>> x0_w = wt3.WeightedTuckerTensorTrain(x0, weights)
+        >>> dense_x = x0_w.to_dense()
+        >>> all_x_vars = x0.tucker_cores + x0.tt_cores + tucker_vectors + tt_vectors
+        >>> einsum_str = 'qix,qjy,qkz,qaib,qbjc,qckd,qi,qj,qk,qa,qb,qc,qd->qxyz'
+        >>> dense_x2 = np.einsum(einsum_str, *all_x_vars)
+        >>> print(np.linalg.norm(dense_x - dense_x2))
+        2.8199489101171104e-12
+        """
+        return self.contract_edge_weights_into_cores(use_jax=use_jax).to_dense(use_jax=use_jax)
+
+
 
 
 if has_jax:
@@ -363,3 +393,210 @@ def wt3_inner_product(
         use_orthogonalization=use_orthogonalization, use_jax=use_jax,
     )
 
+###########################################################################
+########    Scalar valued M.L.F. applies, entries, and probing    #########
+###########################################################################
+
+def wt3_apply(
+        x: WeightedTuckerTensorTrain, # shape=(N0,...,N(d-1))
+        vecs: typ.Sequence[NDArray], # len=d, elm_shape=V+(Ni,)
+        use_jax: bool = False,
+) -> NDArray:
+    '''Contract a Tucker tensor train with vectors in all indices.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> import t3toolbox.tucker_tensor_train as t3
+    >>> import t3toolbox.weighted_tucker_tensor_train as wt3
+    >>> randn = np.random.randn
+    >>> x0 = t3.t3_corewise_randn((6,7,8), (5,6,7), (2,3,3,1), stack_shape=(4,))
+    >>> tucker_vectors = tuple([randn(4, 5), randn(4, 6), randn(4, 7)])
+    >>> tt_vectors = tuple([randn(4, 2), randn(4, 3), randn(4, 3), randn(4, 1)])
+    >>> weights = wt3.EdgeVectors(tucker_vectors, tt_vectors)
+    >>> x = wt3.WeightedTuckerTensorTrain(x0, weights)
+    >>> vecs = [np.random.randn(2,3, 6), np.random.randn(2,3, 7), np.random.randn(2,3, 8)]
+    >>> result = wt3.wt3_apply(x, vecs)
+    >>> result2 = np.einsum('uijk,xyi,xyj,xyk->uxy', x.to_dense(), vecs[0], vecs[1], vecs[2])
+    >>> print(np.linalg.norm(result - result2))
+    6.89832231894826e-13
+    '''
+    return t3.t3_apply(
+        x.contract_edge_weights_into_cores(use_jax=use_jax), vecs,
+        use_jax=use_jax,
+    )
+
+
+
+def wt3_get_entries(
+        x: WeightedTuckerTensorTrain, # shape=(N0,...,N(d-1))
+        index: NDArray, # or convertible to NDArray. dtype=int
+        use_jax: bool = False,
+) -> NDArray:
+    '''Compute an entry (or multiple entries) of a Tucker tensor train.
+
+    This is the entry of the N0 x ... x N(d-1) tensor *represented* by the
+    Tucker tensor train, even though this dense tensor is never formed.
+
+    Parameters
+    ----------
+    x: TuckerTensorTrain
+        Tucker tensor train. shape=(N0,...,N(d-1))
+    index: NDArray or convertible to NDArray, dtype=int
+        Indices of desired entries. shape=(d,)+vsi
+        len(index)=d. If many entries: elm_size=num_entries
+    xnp:
+        Linear algebra backend. Default: np (numpy)
+
+    Returns
+    -------
+    scalar or NDArray
+        Desired entries.shape=(vsi,)
+
+    Raises
+    ------
+    ValueError
+        Error raised if the provided indices in index are inconsistent with each other or the Tucker tensor train x.
+
+    See Also
+    --------
+    TuckerTensorTrain
+    t3_shape
+    t3_apply
+
+    Examples
+    --------
+
+    Compute one entry:
+
+    >>> import numpy as np
+    >>> import t3toolbox.tucker_tensor_train as t3
+    >>> x = t3.t3_corewise_randn((14,15,16), (4,5,6), (1,3,2,1))
+    >>> index = [9, 4, 7] # get entry (9,4,7)
+    >>> result = t3.t3_get_entries(x, index)
+    >>> result2 = x.to_dense()[9, 4, 7]
+    >>> print(np.abs(result - result2))
+    1.3322676295501878e-15
+
+    Compute multiple entries (vectorized):
+
+    >>> import numpy as np
+    >>> import t3toolbox.tucker_tensor_train as t3
+    >>> x = t3.t3_corewise_randn((14,15,16), (4,5,6), (1,3,2,1))
+    >>> index = [[9,8], [4,10], [7,13]] # get entries (9,4,7) and (8,10,13)
+    >>> entries = t3.t3_get_entries(x, index)
+    >>> x_dense = x.to_dense(x)
+    >>> entries2 = np.array([x_dense[9, 4, 7], x_dense[8, 10, 13]])
+    >>> print(np.linalg.norm(entries - entries2))
+    1.7763568394002505e-15
+
+    Vectorize over entries AND T3s
+
+    >>> import numpy as np
+    >>> import t3toolbox.tucker_tensor_train as t3
+    >>> x = t3.t3_corewise_randn((14,15,16), (4,5,6), (2,3,2,2), stack_shape=(2,3))
+    >>> index = [[9,0], [4,0], [7,0]] # get entries (9,4,7) and (0,0,0)
+    >>> entries = t3.t3_get_entries(x, index)
+    >>> x_dense = x.to_dense(x)
+    >>> entries2 = np.moveaxis(np.array([x_dense[:,:, 9,4,7], x_dense[:,:, 0,0,0]]), 0,2)
+    >>> print(np.linalg.norm(entries - entries2))
+    9.22170384909466e-15
+
+    Example using jax jit compiling:
+
+	>>> import numpy as np
+    >>> import jax
+    >>> import t3toolbox.tucker_tensor_train as t3
+    >>> get_entry_123 = lambda x: t3.t3_get_entries(x, (1,2,3), use_jax=True)
+    >>> A = t3.t3_corewise_randn((10,10,10),(5,5,5),(1,4,4,1)) # random 10x10x10 Tucker tensor train
+    >>> a123 = get_entry_123(A)
+    >>> print(a123)
+    -1.3764521
+    >>> get_entry_123_jit = jax.jit(get_entry_123) # jit compile
+    >>> a123_jit = get_entry_123_jit(A)
+    >>> print(a123_jit)
+    -1.3764523
+
+    Example using jax automatic differentiation
+
+    >>> import numpy as np
+    >>> import jax
+    >>> import t3toolbox.tucker_tensor_train as t3
+    >>> import t3toolbox.common as common
+    >>> import t3toolbox.corewise as cw
+    >>> jax.config.update("jax_enable_x64", True) # enable double precision for finite difference
+    >>> get_entry_123 = lambda x: t3.t3_get_entries(x, (1,2,3), use_jax=True)
+    >>> A0 = t3.t3_corewise_randn((10,10,10),(5,5,5),(1,4,4,1)) # random 10x10x10 Tucker tensor train
+    >>> f0 = get_entry_123(A0)
+    >>> G0 = jax.grad(get_entry_123)(A0) # gradient using automatic differentiation
+    >>> dA = t3.t3_corewise_randn((10,10,10),(5,5,5),(1,4,4,1))
+    >>> df = cw.corewise_dot(dA.data, G0.data) # sensitivity in direction dA
+    >>> print(df)
+    -7.418801772515241
+    >>> s = 1e-7
+    >>> A1 = cw.corewise_add(A0.data, cw.corewise_scale(dA.data, s)) # A1 = A0 + s*dA
+    >>> f1 = get_entry_123(t3.TuckerTensorTrain(*A1))
+    >>> df_diff = (f1 - f0) / s # finite difference
+    >>> print(df_diff)
+    -7.418812309825662
+    '''
+    xnp, _, _ = get_backend(False, use_jax)
+
+    index = xnp.array(index)
+
+    if index.shape[0] != x.d:
+        raise ValueError(
+            'Wrong number of indices for Tucker tensor train.\n'
+            + str(x.d) + ' = num tensor indices != num provided indices = ' + str(index.shape[0])
+        )
+
+    return entries.t3_get_entries(x.data, index, use_jax=use_jax)
+
+
+def probe_wt3(
+        ww: typ.Sequence[NDArray], # len=d, elm_shape=W+(Ni,)
+        x: WeightedTuckerTensorTrain, # x.stack_shape=X
+        use_jax: bool=False,
+) -> typ.Sequence[NDArray]: # zz, len=d, elm_shape=X+W+(Ni,)
+    """Probe a TuckerTensorTrain.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> import t3toolbox.tucker_tensor_train as t3
+    >>> import t3toolbox.core.probing as probing
+    >>> x = t3.t3_corewise_randn((10,11,12),(5,6,4),(2,3,4,2))
+    >>> ww = (np.random.randn(10), np.random.randn(11), np.random.randn(12))
+    >>> zz = t3.probe_t3(ww, x)
+    >>> x_dense = x.to_dense()
+    >>> zz2 = probing.probe_dense(ww, x_dense)
+    >>> print([np.linalg.norm(z - z2) for z, z2 in zip(zz, zz2)])
+    [1.0259410400851746e-12, 1.0909087370186656e-12, 3.620283224238675e-13]
+
+    Vectorize over probes:
+
+    >>> import numpy as np
+    >>> import t3toolbox.tucker_tensor_train as t3
+    >>> import t3toolbox.core.probing as probing
+    >>> x = t3.t3_corewise_randn((10,11,12),(5,6,4),(2,3,4,2))
+    >>> ww = (np.random.randn(2,3, 10), np.random.randn(2,3, 11), np.random.randn(2,3, 12))
+    >>> zz = t3.probe_t3(ww, x)
+    >>> x_dense = x.to_dense()
+    >>> zz2 = probing.probe_dense(ww, x_dense)
+    >>> print([np.linalg.norm(z - z2) for z, z2 in zip(zz, zz2)])
+    [2.9290244450205316e-12, 2.0347746956505754e-12, 1.7784156096697445e-12]
+
+    Vectorize over probes and T3s:
+
+    >>> import numpy as np
+    >>> import t3toolbox.tucker_tensor_train as t3
+    >>> import t3toolbox.core.probing as probing
+    >>> x = t3.t3_corewise_randn((10,11,12),(5,6,4),(2,3,4,2), stack_shape=(4,5))
+    >>> ww = (np.random.randn(2,3, 10), np.random.randn(2,3, 11), np.random.randn(2,3, 12))
+    >>> zz = t3.probe_t3(ww, x)
+    >>> x_dense = x.to_dense()
+    >>> zz2 = probing.probe_dense(ww, x_dense)
+    >>> print([np.linalg.norm(z - z2) for z, z2 in zip(zz, zz2)])
+    [1.4471391818397927e-11, 1.0485601346346092e-11, 1.437623640611662e-11]
+    """
+    return probing.probe_t3(ww, x.data, use_jax=use_jax)
